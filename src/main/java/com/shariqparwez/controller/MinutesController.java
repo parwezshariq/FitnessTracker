0bd364ce61070347731610817575f2e9bea61a96
shariqparwez <shariqparwez@outlook.com>
2019-01-25 18:21:12 +0530

    Added Ajax, Content View Negotiator, JQuery -- Final Commit

--- src/main/java/com/shariqparwez/controller/MinutesController.java
+++ src/main/java/com/shariqparwez/controller/MinutesController.java
@@ -1,17 +1,29 @@
 package com.shariqparwez.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shariqparwez.model.Activity;
 import com.shariqparwez.model.Exercise;
+import com.shariqparwez.service.ExerciseService;
 
 @Controller
 public class MinutesController {
 	
+	@Autowired
+	private ExerciseService exerciseService;
+	
 	@RequestMapping(value = "/addMinutes")
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
 		System.out.println("exercise: " + exercise.getMinutes());
+		System.out.println("exercise activity: " + exercise.getActivity());
 		// return "redirect:addMoreMinutes.html";
 		// return "forward:addMoreMinutes.html";
 		return "addMinutes";
@@ -22,4 +34,11 @@ public class MinutesController {
 		System.out.println("exercising: " + exercise.getMinutes());
 		return "addMinutes";
 	}*/
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities(){
+		
+		return exerciseService.findAllAactivities();
+		
+	}
 }
--- src/main/webapp/WEB-INF/jsp/addMinutes.jsp
+++ src/main/webapp/WEB-INF/jsp/addMinutes.jsp
@@ -9,6 +9,28 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Add Minutes Page</title>
+
+<script type="text/javascript" src="jquery-3.3.1.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(
+		function() {
+			$.getJSON('<spring:url value="activities.json"/>', {
+				ajax : 'true'
+			}, function(data){
+				var html = '<option value="">--Please select one--</option>';
+				var len = data.length;
+				for(var i = 0; i < len; i++) {
+					html += '<option value"' + data[i].desc + '">'
+					 + data[i].desc + '</option>';
+				}
+				html += '</option>';
+				
+				$('#activities').html(html);
+			});
+		});
+</script>
+
 </head>
 <body>
 <h1>Add Minutes Exercised</h1>
@@ -20,9 +42,10 @@ Language : <a href="?language=en">English</a> | <a href="?language=es">Spanish</
 		<tr>
 			<td><spring:message code="goal.text" /></td>
 			<td><form:input path="minutes"/></td>
+			<td><form:select id="activities" path="activity"/></td>
 		</tr>
 		<tr>
-			<td colspan="2">
+			<td colspan="3">
 				<input type="submit" value="Enter Exercise"/>
 			</td>
 		</tr>
